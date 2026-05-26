105396cd0d631ff069c5935d786998916e9b9723
jptimmbrum <jptimmbrum@gmail.com>
2026-05-26 16:28:27 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000',
